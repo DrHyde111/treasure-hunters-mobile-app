d4eb490df0c98f543126f124631ab524ede88d0a
DrHyde111 <pawel.j.skalski@gmail.com>
2022-01-29 17:41:50 +0100

    Added missing title property and null exceptions catchers for game model

--- lib/models/game.dart
+++ lib/models/game.dart
@@ -2,16 +2,18 @@ import 'package:treasure_hunters/models/point.dart';
 
 class Game {
   String _id;
+  String title;
   String description;
   String creator;
   List<Point> points;
 
-  Game(this._id, this.description, this.creator, this.points);
+  Game(this._id, this.title, this.description, this.creator, this.points);
 
   Game.fromJson(Map<String, dynamic> json)
       : _id = json['_id'],
-        description = json['description'],
-        creator = json['creator'],
+        title = json['title'] ?? '',
+        description = json['description'] ?? '',
+        creator = json['creator'] ?? '',
         points = List<Point>.from(
             json['points'].map((point) => Point.fromJson(point)));
 }
