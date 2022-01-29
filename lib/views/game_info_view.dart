262bfa1601f918793cc6f5ec61909d499b778793
DrHyde111 <pawel.j.skalski@gmail.com>
2022-01-29 18:23:19 +0100

    Implemented list of all games, and game info views

---
+++ lib/views/game_info_view.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:treasure_hunters/models/game.dart';
+
+class GameInfoView extends StatefulWidget {
+  const GameInfoView({Key? key, required this.token, required this.game})
+      : super(key: key);
+
+  final String token;
+  final Game game;
+
+  @override
+  _GameInfoViewState createState() => _GameInfoViewState();
+}
+
+class _GameInfoViewState extends State<GameInfoView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Game info'),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 50),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      widget.game.title,
+                      style: const TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+              const Text(
+                'Description',
+                textAlign: TextAlign.center,
+              ),
+              Card(
+                margin: EdgeInsets.zero,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(widget.game.description),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                child: Text("Creator"),
+              ),
+              Text(
+                widget.game.creator,
+                style:
+                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/views/games_submenu_view.dart
+++ lib/views/games_submenu_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:treasure_hunters/models/game.dart';
 import 'package:treasure_hunters/services/api_services.dart';
+import 'package:treasure_hunters/views/partial_widgets/submenu_tile.dart';
+
+import 'game_info_view.dart';
 
 class GamesSubmenuView extends StatefulWidget {
   const GamesSubmenuView({Key? key, required this.token}) : super(key: key);
@@ -22,15 +25,47 @@ class _GamesSubmenuViewState extends State<GamesSubmenuView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Main menu'),
-      ),
-      body: SafeArea(
-        child: Column(
-          children: [],
-        ),
-      ),
+    return FutureBuilder<List<Game>>(
+      future: games,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Games'),
+            ),
+            body: SafeArea(
+              child: Column(
+                children: displayGames(snapshot.data),
+              ),
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return Text('${snapshot.error}');
+        }
+
+        // By default, show a loading spinner.
+        return const CircularProgressIndicator();
+      },
     );
+  }
+
+  List<Widget> displayGames(games) {
+    List<Widget> result = [];
+    games.forEach((game) {
+      result.add(TextButton(
+          onPressed: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => GameInfoView(
+                          token: widget.token,
+                          game: game,
+                        )));
+          },
+          child: SubmenuTile(
+            label: game.title,
+          )));
+    });
+    return result;
   }
 }
